519eb2392a6bec3f2d239a1b106ab841db66b5be
Udit Garg <udit15garg@gmail.com>
2026-02-22 02:31:18 +0530

    2.2.26

---
+++ app/build/generated/source/buildConfig/debug/com/palmreader/astro/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.palmreader.astro;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.palmreader.astro";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "4.0";
+  // Field from default config.
+  public static final String OPENAI_API_KEY = "YOUR_API_KEY_HERE";
+}
